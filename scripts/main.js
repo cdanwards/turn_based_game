56935dde595a9a913dd18e78af1eceea2d2bc489
Erik Eggers <em.eggers@gmail.com>
2015-02-06 16:58:04 -0500

    worked on start screen

---
+++ scripts/main.js
@@ -0,0 +1,12 @@
+$(document).ready(function() {
+
+// Audio for start screen
+
+var start = new Audio('sounds/yeah.mp3');
+
+$('.start').on('click', function(){
+  start.play();
+  $(".start-screen-container").addClass("hidden");
+});
+
+});
