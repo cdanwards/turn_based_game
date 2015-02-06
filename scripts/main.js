5f821276a2fe90113e87c01026b0d2b8707dc4dd
Erik Eggers <em.eggers@gmail.com>
2015-02-06 18:36:25 -0500

    update

--- scripts/main.js
+++ scripts/main.js
@@ -1,6 +1,8 @@
 $(document).ready(function() {
 
-// Audio for start screen
+/*
+    Audo for start screen
+*/
 
 var start = new Audio('sounds/yeah.mp3');
 
@@ -8,5 +10,21 @@ $('.start').on('click', function(){
   start.play();
   $(".start-screen-container").hide();
 });
+
+
+
+
+
+
+/*
+  Templete function
+*/
+  function renderTemplate(name, data) {
+    var $template = $('[data-template-name=' + name + ']').text();
+    $.each(data, function(prop, value) {
+      $template = $template.replace('<% ' + prop + ' %>', value);
+    });
+    return $template;
+  }
 
 });
