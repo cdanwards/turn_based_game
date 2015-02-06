27fa018655d7a575a64d84701d7f3bdffd995168
Erik Eggers <em.eggers@gmail.com>
2015-02-06 18:49:06 -0500

    update

--- scripts/main.js
+++ scripts/main.js
@@ -1,53 +1,24 @@
 $(document).ready(function() {
 
 
-  // Code for the start screen
-=======
-/*
-    Audo for start screen
-*/
+// Begin code for the character select screen
+
+// Establishing variables
+var start = new Audio('sounds/yeah.mp3');
+var $characterOutput = $('.character-select');
+
+//Templates
+var renderCharacterTemplate = _.template($('.character-select').html());
 
 
+$('.start').on('click', function(){
+  start.play();
+  $(".start-screen-container").hide();
 
-  // Audio for start screen
-  var start = new Audio('sounds/yeah.mp3');
-
-    $('.start').on('click', function(){
-      start.play();
-      $(".start-screen-container").hide();
-  });
-
-  // Begin code for the character select screen
-
-  // Establishing variables
-  var $characterOutput = $('.character-select');
-
-  //Templates
-  var renderCharacterTemplate = _.template($('.character-select').html());
-
-  var start = new Audio('sounds/yeah.mp3');
-
-  $('.start').on('click', function(){
-    start.play();
-    $(".start-screen-container").hide();
-    function(characterData){
-    $characterOutput.append(renderCharacterTemplate(characterData));
-  });
-
-
-
-
-
-
-/*
-  Templete function
-*/
-  function renderTemplate(name, data) {
-    var $template = $('[data-template-name=' + name + ']').text();
-    $.each(data, function(prop, value) {
-      $template = $template.replace('<% ' + prop + ' %>', value);
-    });
-    return $template;
+  function renderTemplate(characterData){
+  $characterOutput.append(renderCharacterTemplate(characterData));
   }
+});
+
 
 });
