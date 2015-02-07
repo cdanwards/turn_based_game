351be8f8131917c7fdd1d3988a1afd4ea8797a11
Erik Eggers <em.eggers@gmail.com>
2015-02-06 22:34:39 -0500

    update

--- scripts/main.js
+++ scripts/main.js
@@ -13,7 +13,6 @@ var renderCharacterTemplate = _.template($('.character-select').html());
 
 $('.start').on('click', function(){
   start.play();
-  alert('clicked');
   $(".start-screen-container").hide();
 
   function renderTemplate(characterData){
@@ -24,11 +23,44 @@ $('.start').on('click', function(){
 
 
 // Character Select page
-$characterOuput.on('click', '.mordecai', function() {
-  alert('clicked');
-  $('.selected-character').addClass('mordecai');
+
+// $characterOutput.on('click', '.mordecai', function() {
+//   alert('clicked');
+//   $('.selected-character').addClass('mordecai-background');
+// });
+//
+// $characterOutput.on('click', '.rigby', function() {
+//   alert('clicked');
+//   $('.selected-character').addClass('rigby-background');
+// });
+//
+// $characterOutput.on('click', '.benson', function() {
+//   alert('clicked');
+//   $('.selected-character').addClass('benson-background');
+// });
+//
+// $characterOutput.on('click', '.benson', function() {
+//   alert('clicked');
+//   $('.selected-character').addClass('benson-background');
+// });
+//
+// $characterOutput.on('click', '.pops', function() {
+//   alert('clicked');
+//   $('.selected-character').addClass('pops-background');
+// });
+//
+//
+// $characterOutput.on('click', '.skips', function() {
+//   alert('clicked');
+//   $('.selected-character').addClass('skips-background');
+// });
+
+var characterNames = ['mordecai', 'rigby', 'benson', 'pops', 'skips', 'muscleman'];
+characterNames.forEach(function(char) {
+    $characterOutput.on('click', '.'+char, function() {
+        // alert('click on ' + char + '!');
+        $('.selected-character').addClass(char + '-background');
+    });
 });
-
-
 
 });
