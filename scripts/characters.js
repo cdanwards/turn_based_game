73bcd82e9aacb0d6223de4a556c210d8e549ed33
Erik Eggers <em.eggers@gmail.com>
2015-02-07 11:40:54 -0500

    update

---
+++ scripts/characters.js
@@ -0,0 +1,36 @@
+(function(){
+  'use strict';
+
+var Character = function (options) {
+  options = options || {};
+  _.deafaults(options, {
+    health: 100,
+    strength: 10
+  });
+};
+
+var mordecaiCharacter = new Character({
+
+});
+
+var rigbyCharacter = new Character({
+
+});
+
+var bensonCharacter = new Character({
+
+});
+
+var popsCharacter = new Character({
+
+});
+
+var skipsCharacter = new Character({
+
+});
+
+var muscleManCharacter = new Character({
+
+});
+
+})();
--- scripts/main.js
+++ scripts/main.js
@@ -1,6 +1,7 @@
+(function(){
+  'use strict';
+
 $(document).ready(function() {
-
-
 
 // Sounds
 var start = new Audio('sounds/yeah.mp3');
@@ -11,15 +12,13 @@ var pops = new Audio('sounds/pops.mp3');
 var skips = new Audio('sounds/skips.mp3');
 var muscleman = new Audio('sounds/muscleman.mp3');
 
-
-
-// Character select
+// Character select div
 var $characterOutput = $('.character-screen');
 
 //Template
 var renderCharacterTemplate = _.template($('.character-select').html());
 
-
+// Character select screen
 $('.start').on('click', function(){
   start.play();
   $(".theme-song").detach();
@@ -31,9 +30,6 @@ $('.start').on('click', function(){
   renderTemplate();
 });
 
-
-// Character Select page
-
 // $characterOutput.on('click', '.mordecai', function() {
 //   // alert('clicked');
 //   $('.selected-character').addClass('mordecai-background');
@@ -43,29 +39,7 @@ $('.start').on('click', function(){
 //   // alert('clicked');
 //   $('.selected-character').addClass('rigby-background');
 // });
-//
-// $characterOutput.on('click', '.benson', function() {
-//   // alert('clicked');
-//   $('.selected-character').addClass('benson-background');
-// });
-//
-// $characterOutput.on('click', '.pops', function() {
-//   // alert('clicked');
-//   $('.selected-character').addClass('pops-background');
-// });
-//
-//
-// $characterOutput.on('click', '.skips', function() {
-//   // alert('clicked');
-//   $('.selected-character').addClass('skips-background');
-// });
-//
-// $characterOutput.on('click', '.muscleman', function() {
-//   // alert('clicked');
-//   $('.selected-character').addClass('muscleman-background');
-// });
 var characterNames = ["mordecai", "rigby", "benson", "pops", "skips", "muscleman"];
-// var lastClass = $('#container').attr('class').split(' ').pop();
 
 characterNames.forEach(function(char) {
     $characterOutput.on('click', '.'+char, function(e) {
@@ -79,3 +53,4 @@ characterNames.forEach(function(char) {
 
 
 });
+})();
